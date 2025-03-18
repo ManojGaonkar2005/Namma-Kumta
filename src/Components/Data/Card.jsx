cf6325760ad8448fa346937246f777ed31d01ccf
ManojGaonkar2005 <manojs200511@gmail.com>
2025-03-18 20:14:30 +0530

    Update in Photograpy and videography component datas

--- src/Components/Data/Card.jsx
+++ src/Components/Data/Card.jsx
@@ -12,7 +12,8 @@ export const MakeupData = [
     title: "Soundarya Beauty Parlour",
     info: "Sumuka nilaya, mark compound,Halkar Kumta, Karnataka 581343",
     rating: "4.7",
-  },{
+  },
+  {
     id: 3,
     image: "./Images/h2.png",
     title: "Soundarya Beauty Parlour",
@@ -23,24 +24,69 @@ export const MakeupData = [
     id: 4,
     image: "./Images/h2.png",
     title: "Soundarya Beauty Parlour",
-    info: "Sumuka nilaya, mark compound,Halkar Kumta, Karnataka 581343",
+    info: "Sumuka nilaya, mark compound,Halkar Kumta, Karnataka",
+    rating: "4.7",
+  }, {
+    id: 5,
+    image: "./Images/h2.png",
+    title: "Soundarya Beauty Parlour",
+    info: "Sumuka nilaya, mark compound,Halkar Kumta, Karnataka",
+    rating: "4.7",
+  }, {
+    id: 6,
+    image: "./Images/h2.png",
+    title: "Soundarya Beauty Parlour",
+    info: "Sumuka nilaya, mark compound,Halkar Kumta, Karnataka",
     rating: "4.7",
   },
 ];
 export const PhotoData = [
   {
     id: 1,
-    image: "./Images/p1.png",
-    title: "Shripad hegde Photography",
-    info: "bab Complex, NH 66, opp. Gibb Primary School, Kumta, Karnataka 581343",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipOxIUcboWBjiAJWLIP_MvfOyi7pK20AeZ9KocI3=w114-h114-n-k-no",
+    title: "Ninada Digital Studio",
+    info: "Old Fish Market Rd, Kumta, Karnataka",
     rating: "4.7",
   },
   {
     id: 2,
-    image: "./Images/p2.png",
-    title: "Shri Matha Photography",
-    info: "Masthi Katte Circle, Kumta, Karnataka 581343",
-    rating: "4.6",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipPk5idbpgbwHW__8EAjAVZ8newCvf9yOoCW1uE3=w114-h114-n-k-no",
+    title: "DEEKSHA Digital Photo Studio",
+    info: "Bhandari Complex, Murukatte, Kumta, Karnataka",
+    rating: "2.7",
+  },
+  {
+    id: 3,
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipNmu2y9RZtzx6dNsGQA1pei2-o02nSYhPZ0Q6tA=w114-h114-n-k-no",
+    title: "Melmak Studio",
+    info: "Mahabaleshwar Complex, Kumbeshwar Road, Kumta, Karnataka",
+    rating: "4.8",
+  },
+  {
+    id: 4,
+    image:
+      "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=Ayy3J9oAoqKv8A5T3xzK7g&cb_client=geo_services_immersive.gps&w=144&h=144&yaw=18.60424&pitch=0&thumbfov=100",
+    title: "Shree Durga studio",
+    info: "Baggon Rd, Baggon, Kumta, Karnataka 581332",
+    rating: "5.0",
+  },
+  {
+    id: 5,
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipNtzFjc4fGU2fWKUIERLtCicoVtrBguujJf0LBT=w114-h114-n-k-no",
+    title: "Shri matha photography",
+    info: "Masthi katte circle, Kumta, Karnataka 581343",
+    rating: "4.8",
+  },
+  {
+    id: 6,
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipNyl3QnGHdAdJxmtF4Z8PtzgE60IRjKrU-iIexQ=w114-h114-n-k-no",
+    title: "Royal Photo Service",
+    info: "Vijaya Laxmi Complex, Road, Kumta, Karnataka",
+    rating: "4.8",
   },
 ];
-
